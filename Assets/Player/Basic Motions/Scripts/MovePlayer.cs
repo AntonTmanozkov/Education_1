eab960f21eed162d727dd00f33767246c955e8b5
Просвирин Антон <at@panacotagames.com>
2024-07-14 16:32:55 +0300

    Сделал передвижение и прыжок игроку, немножко повозился с камерой

--- Assets/Player/Basic Motions/Scripts/MovePlayer.cs
+++ Assets/Player/Basic Motions/Scripts/MovePlayer.cs
@@ -1,14 +1,63 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class MovePlayer : MonoBehaviour
 {
-    // скрипт не дработан. Нужно скорее всего создавать ещё один для создания префаба
-    [field: SerializeField] private GameObject Player_PREFAB;
     [field: SerializeField] private float _speed = 0.5f;
+    [field: SerializeField] private float _jumpForce = 300f;
 
-    private Rigidbody2D _PlayerRb;
+    private Rigidbody _playerRb;
+    private bool _isGrounded;
 
-    private void Update() {
-        _PlayerRb = GetComponent<Rigidbody2D>();
+    private void Awake() 
+    {
+        _playerRb = GetComponent<Rigidbody>();
     }
+
+    private void FixedUpdate() 
+    {
+        MovementLogic();
+        JumpLogic();
+    }
+
+    private void MovementLogic()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        _playerRb.AddForce(movement * _speed);
+    }
+
+    private void JumpLogic()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (_isGrounded)
+            {
+                _playerRb.AddForce(Vector3.up * _jumpForce);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        IsGroundedUpdate(other, true);
+    }
+
+    private void OnCollisionExit(Collision other) {
+        IsGroundedUpdate(other, false);
+    }
+
+    private void IsGroundedUpdate(Collision collision, bool value)
+    {
+        if (collision.gameObject.tag == "Ground") 
+        {
+            _isGrounded = value;
+        } 
+        
+    }
+
+    // public void GetPlayer(GameObject Player) => _player = Player;
 }
